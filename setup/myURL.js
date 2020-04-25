ecd271df8bf33f9dd127fde9742b2180a48fc8b6
ZeesahanAhmad <54854866+ZeesahanAhmad@users.noreply.github.com>
2020-04-25 11:08:34 +0530

    password hide

--- setup/myURL.js
+++ setup/myURL.js
@@ -1,4 +1,4 @@
 module.exports={
-    mongoURL:"mongodb+srv://zeeshan:zeeshan123@cluster0-sgtpb.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURL:"mongodb+srv://zeeshan:password@cluster0-sgtpb.mongodb.net/test?retryWrites=true&w=majority",
     secret:"veryStrongSecret"
 };
